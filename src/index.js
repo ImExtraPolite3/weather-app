26272c1b965ab8cf5eff36b08f536d13183da37d
ExtraPolite <imextrapolite2@gmail.com>
2024-06-27 05:47:56 -0400

    test using factory fuction with async function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import './style.css';
 
+function test(city_name, country_name) {
+  return { city_name, country_name };
+}
+
 async function weatherInfo(location) {
   const getInfo = await fetch('https://api.weatherapi.com/v1/current.json?key=a9fa1babbb44465cbd753452242606&q=' + location, {mode: 'cors'})
     .then(function(response) {
       return response.json();
     })
     .then(function(response) {
-      console.log(response.location.name);
+      const testSomething = test(response.location.name, response.location.country);
+
+      console.log(testSomething.city_name);
+    })
+    .catch(() => {
+      console.log('please enter city name');
     });
 }
 
