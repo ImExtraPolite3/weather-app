9848d300ddfd9907d6a6605eaaec9cbd690ad4ea
ExtraPolite <imextrapolite2@gmail.com>
2025-02-14 20:49:09 -0500

    link api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState, useEffect } from 'react';
+
 function App() {
-  return (
-    <>
-      <p>hello</p>
-    </>
-  );
+  const [getApi, setApi] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://api.weatherapi.com/v1/forecast.json?key=a9fa1babbb44465cbd753452242606&q=London'
+    )
+      .then((response) => response.json())
+      .then((json) => setApi(json))
+      .catch((error) => console.log(error));
+  });
+
+  return <>{getApi !== null && <p>{getApi.location.name}</p>}</>;
 }
 
 export default App;
