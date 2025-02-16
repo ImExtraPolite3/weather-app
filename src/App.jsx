8f99778dfed67a5c6176f1ee4ab4cbb2641d427f
ExtraPolite <imextrapolite2@gmail.com>
2025-02-15 20:55:51 -0500

    make basic outline

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [getApi, setApi] = useState(null);
+  const [getLocation, setLocation] = useState('London');
 
   useEffect(() => {
     fetch(
-      'https://api.weatherapi.com/v1/forecast.json?key=a9fa1babbb44465cbd753452242606&q=London'
+      `https://api.weatherapi.com/v1/forecast.json?key=a9fa1babbb44465cbd753452242606&q=${getLocation}`
     )
       .then((response) => response.json())
       .then((json) => setApi(json))
       .catch((error) => console.log(error));
   });
 
-  return <>{getApi !== null && <p>{getApi.location.name}</p>}</>;
+  return (
+    <>
+      {getApi !== null && (
+        <div className="container">
+          <div className="search"></div>
+          <div className="content">
+            <p>{getApi.location.name}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
